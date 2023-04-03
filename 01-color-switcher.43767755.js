46b9ddc4a35a928b0c1cac05e8c151f2e7b1d0ab
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2023-04-03 12:32:38 +0000

    Deploying to gh-pages from @ Elborn666/goit-js-hw-09@07c19297929bb2a49a22127cec4f203c5b1405d2 🚀

---
+++ 01-color-switcher.43767755.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let a=null;e.setAttribute("disabled",""),t.addEventListener("click",(t=>{t.target.setAttribute("disabled",!0),e.removeAttribute("disabled"),a=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(e=>{e.target.setAttribute("disabled",!0),t.removeAttribute("disabled"),clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.43767755.js.map
