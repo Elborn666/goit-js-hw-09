07c19297929bb2a49a22127cec4f203c5b1405d2
Elborn666 <108280320+Elborn666@users.noreply.github.com>
2023-04-03 15:32:12 +0300

    task 01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const btnStart = document.querySelector('button[data-start]')
+const btnStop = document.querySelector('button[data-stop]')
+const bodyColor = document.querySelector('body')
+let timerId = null;
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+btnStop.setAttribute('disabled', '');
+
+
+btnStart.addEventListener('click', element => {
+     element.target.setAttribute('disabled', true);
+     btnStop.removeAttribute('disabled');
+
+timerId = setInterval(() => {
+    bodyColor.style.backgroundColor = getRandomHexColor();
+    }, 1000);    
+});
+
+
+btnStop.addEventListener('click', element => {
+  element.target.setAttribute('disabled', true);
+  btnStart.removeAttribute('disabled');
+
+  clearInterval(timerId);
+});
